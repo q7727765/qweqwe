facb8ee48aca4820c3d5f632084c4a2c5c371e90
arctory <504306313@qq.com>
2017-02-24 16:35:23 +0800

    加入led函数功能

--- USER/test.c
+++ USER/test.c
@@ -7,12 +7,19 @@
 //技术支持：www.openedv.com
 //广州市星翼电子科技有限公司
   
+void led()
+{
+	
+}
 int main(void)
 { 
 	Stm32_Clock_Init(336,8,2,7);//设置时钟,168Mhz
 	delay_init(168);		//初始化延时函数
 	LED_Init();				//初始化LED时钟  
 	BEEP_Init();         	//初始化蜂鸣器端口
+	
+	
+	
 	while(1)
 	{
 		LED0=0;
